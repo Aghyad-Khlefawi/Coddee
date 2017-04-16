9664fb5803e76f7e581107f81f16ec701b59f32d
Aghyad Khlefawi <Aghyad.khlefawi@hotmail.com>
2017-04-16 11:50:33 +0300

    Versions fix

--- src/Coddee.AspNet/Properties/AssemblyInfo.cs
+++ src/Coddee.AspNet/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("37cce7b1-aad7-4d4a-a783-d7dadeb7f3b7")]
 
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.1.0")]
